fbdc188f09548646ead725ccb7673cb2c86d7291
Pranshu1902 <pranshu1902@gmail.com>
2022-08-06 21:01:09 +0530

    updated preview pae

--- src/Components/PublicPreview.tsx
+++ src/Components/PublicPreview.tsx
@@ -2,7 +2,6 @@ import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getPublicCards, getPublicLinks } from "../api/ApiUtils";
 import { card, link, publicCard, publicLink } from "../Common/DataType";
-import Header from "../Common/Header";
 
 export default function PublicPreview(props: {
   userId: number;
@@ -46,82 +45,83 @@ export default function PublicPreview(props: {
 
   return (
     <div>
-      <Header tab={"Preview"} />
-      {loading ? (
-        <div className="flex justify-center items-center pt-12">
-          <CircularProgress color="secondary" />
-        </div>
-      ) : (
-        <div className="p-4 flex justify-center items-center pt-6">
-          {card && (
-            <div
-              className={`${
-                card.color === "purple"
-                  ? "bg-purple-100 text-[#4c00b0]"
-                  : card.color === "blue"
-                  ? "bg-blue-100 text-blue-600"
-                  : "bg-red-100 text-red-600"
-              } p-4 rounded-lg w-fit`}
-            >
-              <div className="flex flex-row gap-4">
+      <div className="flex justify-center items-center min-h-screen bg-gradient-to-tr from-purple-200 to-purple-400">
+        {loading ? (
+          <div className="flex justify-center items-center pt-12">
+            <CircularProgress color="secondary" />
+          </div>
+        ) : (
+          <div className="p-4 flex justify-center items-center pt-6">
+            {card && (
+              <div
+                className={`${
+                  card.color === "purple"
+                    ? "bg-purple-100 text-[#4c00b0]"
+                    : card.color === "blue"
+                    ? "bg-blue-100 text-blue-600"
+                    : "bg-red-100 text-red-600"
+                } p-4 rounded-lg w-fit`}
+              >
+                <div className="flex flex-row gap-4">
+                  <div>
+                    {/* <img src="" alt="" /> */}
+                    <i className=" text-7xl fa fa-user"></i>
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <div>
+                      <p className="text-3xl font-bold">{card.name}</p>
+                      <p className="text-xl">{card.title}</p>
+                    </div>
+                  </div>
+                </div>
+                <div>{card.description}</div>
                 <div>
-                  {/* <img src="" alt="" /> */}
-                  <i className=" text-7xl fa fa-user"></i>
+                  {card.email.length > 0 && (
+                    <div>
+                      <i className="fa fa-envelope"></i> {card.email}
+                    </div>
+                  )}
+                  {card.phone.length > 0 && (
+                    <div>
+                      <i className="fa fa-phone"></i> {card.phone}
+                    </div>
+                  )}
+                  {card.location.length > 0 && (
+                    <div>
+                      <i className="fa fa-map-marker-alt"></i> {card.location}
+                    </div>
+                  )}
                 </div>
-                <div className="flex flex-col gap-4">
-                  <div>
-                    <p className="text-3xl font-bold">{card.name}</p>
-                    <p className="text-xl">{card.title}</p>
-                  </div>
+                <div className="flex gap-2">
+                  {links && links.length > 0 && (
+                    <div className="flex gap-2">
+                      {links.map((link: link) => {
+                        return (
+                          <a
+                            href={link.link}
+                            target={"_blank"}
+                            rel={"noreferrer"}
+                          >
+                            <i
+                              className={`${link.icon} ${
+                                card.color === "purple"
+                                  ? "bg-purple-200 hover:bg-purple-300"
+                                  : card.color === "blue"
+                                  ? "bg-blue-200 hover:bg-blue-300"
+                                  : "bg-red-200 hover:bg-red-300"
+                              } hover:bg-purple-300 hover:scale-105 transition duration-300 rounded-full p-2`}
+                            ></i>
+                          </a>
+                        );
+                      })}
+                    </div>
+                  )}
                 </div>
               </div>
-              <div>{card.description}</div>
-              <div>
-                {card.email.length > 0 && (
-                  <div>
-                    <i className="fa fa-envelope"></i> {card.email}
-                  </div>
-                )}
-                {card.phone.length > 0 && (
-                  <div>
-                    <i className="fa fa-phone"></i> {card.phone}
-                  </div>
-                )}
-                {card.location.length > 0 && (
-                  <div>
-                    <i className="fa fa-map-marker-alt"></i> {card.location}
-                  </div>
-                )}
-              </div>
-              <div className="flex gap-2">
-                {links && links.length > 0 && (
-                  <div className="flex gap-2">
-                    {links.map((link: link) => {
-                      return (
-                        <a
-                          href={link.link}
-                          target={"_blank"}
-                          rel={"noreferrer"}
-                        >
-                          <i
-                            className={`${link.icon} ${
-                              card.color === "purple"
-                                ? "bg-purple-200 hover:bg-purple-300"
-                                : card.color === "blue"
-                                ? "bg-blue-200 hover:bg-blue-300"
-                                : "bg-red-200 hover:bg-red-300"
-                            } hover:bg-purple-300 hover:scale-105 transition duration-300 rounded-full p-2`}
-                          ></i>
-                        </a>
-                      );
-                    })}
-                  </div>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      )}
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
