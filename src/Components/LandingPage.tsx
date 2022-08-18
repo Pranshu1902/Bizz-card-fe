df46852f072a7f22ba243f73abd2b32ca7c9e6b6
Pranshu1902 <pranshu1902@gmail.com>
2022-08-18 19:35:46 +0530

    added portfolio site link

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -9,23 +9,33 @@ export default function LandingPage() {
   }, []);
 
   return (
-    <div className="">
+    <div>
       <Header tab={"LandingPage"} />
-      <div className="absolute">
+      <div className="bg-purple-200 min-h-screen">
         <div className="flex flex-col-reverse gap-4 justify-center items-center p-4">
           <div className="md:w-1/3">
             <img src={home} className="rounded-xl" alt="" />
           </div>
           <p
             style={{ fontFamily: "Times New Roman, Times, serif" }}
-            className="flex justify-center text-white font-mono font-bold text-5xl"
+            className="flex justify-center text-purple-800 font-mono font-bold text-5xl"
           >
             Create your identity today by using our simple and easy to use
             website to leave an amazing first impression.
           </p>
         </div>
+        <div className="flex justify-center items-center text-purple-700 pt-16">
+          Made with &nbsp;<i className="fa fa-heart"></i>&nbsp; by&nbsp;
+          <a
+            href="https://pranshu1902.netlify.app"
+            target={"_blank"}
+            rel={"noreferrer"}
+            className="hover:text-purple-900 hover:font-bold transition duration-300 underline"
+          >
+            Pranshu
+          </a>
+        </div>
       </div>
-      <img className="w-screen h-screen" src={bg} alt="" />
     </div>
   );
 }
